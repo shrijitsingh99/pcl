bd3402d3554eac0a76a4da73cd20deb5a755c778
Shrijit Singh <shrijitsingh99@gmail.com>
2020-07-01 20:46:58 +0530

    Add executor tests

--- common/include/pcl/experimental/execution/executor.hpp
+++ common/include/pcl/experimental/execution/executor.hpp
@@ -1,7 +1,9 @@
 #pragma once
 
-#include <pcl/experimental/execution//executor/base_executor.hpp>
-#include <pcl/experimental/execution//executor/cuda_executor.hpp>
-#include <pcl/experimental/execution//executor/inline_executor.hpp>
-#include <pcl/experimental/execution//executor/omp_executor.hpp>
-#include <pcl/experimental/execution//executor/sse_executor.hpp>
+#include <pcl/experimental/execution/executor/base_executor.hpp>
+#include <pcl/experimental/execution/executor/cuda_executor.hpp>
+#include <pcl/experimental/execution/executor/inline_executor.hpp>
+#include <pcl/experimental/execution/executor/omp_executor.hpp>
+#include <pcl/experimental/execution/executor/sse_executor.hpp>
+
+using best_fit = inline_executor<oneway_t, single_t, blocking_t::always_t>;
